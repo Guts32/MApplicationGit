f9e735759536d2321401a19af7961e698ce10096
PC <bryanngustavo2-0@gmail.com>
2020-11-25 14:56:05 -0600

    Agregar Activty Login Creacion de TapLayout

--- app/src/main/java/com/myapp/myapplicationgit/Login_Tap_Fragment.java
+++ app/src/main/java/com/myapp/myapplicationgit/Login_Tap_Fragment.java
@@ -1,24 +1,41 @@
 package com.myapp.myapplicationgit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Login_Tap_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Login_Tap_Fragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthMultiFactorException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.MultiFactorResolver;
+
+
+public class Login_Tap_Fragment extends Fragment{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
+    View v;
+    private FirebaseAuth mAuth;
+    private EditText mEmail,mPassword;
+    Button login;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -28,37 +45,67 @@ public class Login_Tap_Fragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Login_Tap_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Login_Tap_Fragment newInstance(String param1, String param2) {
-        Login_Tap_Fragment fragment = new Login_Tap_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+
+    private void attachToXML(){
+        try {
+
+            mAuth = FirebaseAuth.getInstance();
+            mEmail = v.findViewById(R.id.email);
+            mPassword = v.findViewById(R.id.password);
+            login = v.findViewById(R.id.login);
+
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Login_User();
+                }
+            });
+        }
+        catch (Exception e){
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void Login_User(){
+        if(!mEmail.getText().toString().isEmpty() && mPassword.getText().toString().isEmpty()){
+
+            if (mAuth != null)
+            {
+                mAuth.signInWithEmailAndPassword(mEmail.getText().toString(),mPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                startActivity(new Intent(getActivity().getApplicationContext(), MainActivity.class) );
+                                getActivity().finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+        else{
+            Toast.makeText(getContext(), "Complete los campos", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login__tap_, container, false);
+        v = inflater.inflate(R.layout.fragment_login__tap_, container, false);
+        return v;
+
     }
+
+
+
 }
--- app/src/main/java/com/myapp/myapplicationgit/MainActivity.java
+++ app/src/main/java/com/myapp/myapplicationgit/MainActivity.java
@@ -2,13 +2,36 @@ package com.myapp.myapplicationgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.Salir);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAuth != null){
+                    mAuth.signOut();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        });
     }
 }
